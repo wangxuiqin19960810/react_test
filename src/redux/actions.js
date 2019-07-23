bafa5cbd40d90bc6504532f817433c5bbc77918a
xiuqin <2023144348@qq.com>
2019-07-23 10:16:09 +0800

    111

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -19,19 +19,17 @@ export const decrement = (number)=>({type:DECREMENT,number})
 /* 
 创建一个异步增加的action
 异步action是一个函数，参数是dispatch函数
-分发异步action是为了有机会执行异步操作
+分发异步action是为了有机会执行异步代码
     1.执行异步代码
     2.有了结果后，分发同步action
  */
 
-export const incrementAsync = (number)
-    return (dispatch)=>{
+export const incrementAsync = (number) =>(
+    dispatch =>{
         //执行异步代码
         setTimeout(() => {
             //有了结果以后，分发同步action
             dispatch(incrementAsync(number))
-        }, 1000);
-
-        
+        }, 2000)
     }
 )
